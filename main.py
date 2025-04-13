290cf1b34e7d81e7d4bbc1f4dab50dfb1cae53ee
EugeniaBel <belohay@gmail.com>
2025-04-13 15:59:01 +0200

    upper_lower testing

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-def round6(num: float):
-    """This function has a bug in it"""
-    return int(num + .6)
+
+#def round6(num: float):
+    #"""This function has a bug in it"""
+    #return int(num + .6)
+
+
+def upper_lower(text):
+    """
+    Checks if a given string contains an occurrence
+    of upper case letter followed by lower case letter.
+    Returns True if such thing exists, False otherwise.
+    """
+    for i in range(len(text) - 1):
+        if text[i].isupper():
+            if text[i+1].islower():
+                return True
+    return False
+
+# ---- automated unit test ----
+
+assert upper_lower("lowlow") == False
+assert upper_lower("blaBla") == True
+assert upper_lower("blabLa") == True
+assert upper_lower("byE") == False
+assert upper_lower("Hello") == True
+assert upper_lower("BOOM") == False
+assert upper_lower("") == False
+
+print("All tests passed!")
